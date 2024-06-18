d53853ab83dce1d2fab6de4d555baeccaae71caf
LiuShuojiang <shuojiang@gatech.edu>
2024-06-18 12:35:34 -0400

    Add lab3 implementation: speed up system calls

--- kernel/proc.h
+++ kernel/proc.h
@@ -95,6 +95,9 @@ struct proc {
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
+  // Lab3: page table ------ speed up system calls
+  struct usyscall *usyscallPage;
+
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
