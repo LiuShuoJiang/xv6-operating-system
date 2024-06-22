5cca3cad15e6dd5996f4b63f4569a5b3c17bfbc6
LiuShuojiang <shuojiang@gatech.edu>
2024-06-22 18:26:38 -0400

    Add lab4 implementation: RISC-V assembly

--- user/call.c
+++ user/call.c
@@ -13,5 +13,13 @@ int f(int x) {
 
 void main(void) {
   printf("%d %d\n", f(8)+1, 13);
+
+  // // Lab4: traps ------ RISC-V assembly
+  unsigned int i = 0x00646c72;
+	printf("H%x Wo%s\n", 57616, &i);  // HE110 World
+
+  int c1 = 0x64, c2 = 0x6c, c3 = 0x72;
+  printf("%c %c %c %x\n", c1, c2, c3, 57616);  // d l r
+
   exit(0);
 }
